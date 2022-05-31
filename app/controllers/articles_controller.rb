8060562ef22ab0d2203ee3da8ec9cdcb1c82504d
meach <meach.c@gmail.com>
2022-05-31 09:53:14 -0700

    added creation end points

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,28 @@
 class ArticlesController < ApplicationController
+    #defined as :show in routes.rb
     def show
         #params are what is passed in url bar, the variable is :id
         @article = Article.find(params[:id])
+    end
+
+
+    #when this is made, you need a new view. (Much like show.html.erb in views/articles/  In this case we make index.html.erb)
+    def index
+        #@articles is an isntance variable which is available in views. (same as above)
+        @articles = Article.all
+    end
+
+    def new
+
+    end
+
+    def create
+        #render plain: params[:article]
+        #permits the title and description from the article to be saved...
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        #this is how you make a redirect...  Find the path in endpoints and append _path to it.
+        #can also shorten this to redirect_to @article
+        redirect_to article_path(@article)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  # rails routes --expanded [[To check the routes in console]]
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index, :new, :create]
 
 end
